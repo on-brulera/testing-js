dc4d15ca46fa603aced79a51b1961c49eff329e6
on-brulera <luishenryramirezmontalvo746@gmail.com>
2023-08-10 17:02:41 -0500

    Conceptos objeto Math

---
+++ funtions-objects/math.js
@@ -0,0 +1,27 @@
+//Objeto Math
+
+console.log(Math);
+
+//constantes
+console.log(Math.PI);
+
+//valor absoluto
+console.log(Math.abs(-7.8));
+
+//redondear a entero al numero mayor
+console.log("ceil:", Math.ceil(6.2));
+
+//redonodear a entero menor
+console.log("floor:", Math.floor(6.2));
+
+//redondear generico
+console.log("round:", Math.round(7.2));
+
+//sign da -1 (negativo) +1 (positivo) o 0
+console.log("sign:", Math.sign(100));
+
+//numero aleatorio entre 0 y 1
+console.log("random:", Math.random());
+
+//numero entre 1 y 100
+console.log("1-1000:", Math.round(Math.random() * 1000));
