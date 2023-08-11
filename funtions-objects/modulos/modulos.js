9f6324ac31966ea43218bdd6496fb92e97fe6df0
on-brulera <luishenryramirezmontalvo746@gmail.com>
2023-08-11 08:22:15 -0500

    importar y exportar en js

---
+++ funtions-objects/modulos/modulos.js
@@ -0,0 +1,9 @@
+import saludar, { PI } from "./constantes.js";
+import { suma, resta } from "./aritmetica.js";
+
+console.log("Archivo modulo");
+console.log("Valor PI:", PI);
+console.log("Suma", suma(1, 1));
+console.log("Resta", resta(2, 1));
+
+saludar();
